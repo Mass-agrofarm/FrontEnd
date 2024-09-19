a1a006b55eb3069212ac5b5cf49f92364275be8d
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-09-19 18:57:46 +0300

    Feat: Made changes to the product cards styling, added ProductCardsContainer

--- src/Components/ProductComponents/ProductCards.jsx
+++ src/Components/ProductComponents/ProductCards.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import './productStyles.css'
+// eslint-disable-next-line no-unused-vars
+import React , {useState, Suspense} from 'react';
+import './productStyles.css';
 
 
 const ProductCards = () => {
   return (
     <div className="cards-container">
-      {
-        <div className="card" style={{width: '355px', backgroundColor: 'whitesmoke', color: 'black', padding: '5px', borderRadius: '15px'}}>
-          <div className="top">
-            <img className='img' src='https://fakeimg.pl/353x252/c5c5c5/959595?text=Mass+Agro+Farm+Product&font=museo&font_size=24&retina=1' alt='Mass Agrofarm product'/>
-            <h3>Product Name.</h3>
+          <div className="product-card">
+            <div className="product-top">
+              <img className='product-img' src='https://fakeimg.pl/353x252/c5c5c5/959595?text=Mass+Agro+Farm+Product&font=museo&font_size=24&retina=1' alt='Mass Agro farm product'/>
+              <h3>product name.</h3>
+            </div>
+            <div className='product-bottom'>
+              <p className='description'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem reprehenderit illum numquam. Eum quaerat sit laboriosam inventore laborum distinctio beatae nesciunt quibusdam</p>
+              <div className="product-enquire-button">
+                <button className="product-button">Enquire</button>
+              </div>
+            </div>
           </div>
-          <div className='bottom' style={{ padding:'0px 0px 15px 15px' }}>
-            <p className='description'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem reprehenderit illum numquam. Eum quaerat sit laboriosam inventore laborum distinctio beatae nesciunt quibusdam</p>
-          </div>
-        </div>
-      }
     </div>
   )
-}
+};
 
 export default ProductCards
