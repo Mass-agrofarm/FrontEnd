4f5b4cbbf9937fc700cbfe52a3a7a713971568b6
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-09-19 19:01:29 +0300

    Started working on the contacts component

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { FlexDiv, OuterContainer } from '../Components/StyledComponents/StyledComponents'
 
 function Contacts (){
   return (
     <>
-      <h1>Let us know how we can reach you.</h1>
-      <form>customer details</form>
-      <testimonials>customer satisfaction and rating.</testimonials>
-      <faq></faq>
+      <FlexDiv>
+        <h1>Let us know how we can reach you.</h1>
+        <form>customer details</form>
+        <testimonials>customer satisfaction and rating.</testimonials>
+        <faq></faq>
+      </FlexDiv>
     </>
   )
 }
