0601615b0358dd339cf44a9c5b046a72ed68afe8
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-09-19 18:58:51 +0300

    Bug: Solved the display issues on the main container for the product cards

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,18 +1,50 @@
-import React from 'react'
+// dependancies and tools
+// eslint-disable-next-line no-unused-vars
+import React, { Suspense, useState } from 'react';
+import styled from 'styled-components';
 
 //components
-import ProductCards from '../Components/ProductComponents/ProductCards'
+import ProductCards from '../Components/ProductComponents/ProductCards';
 
 // custom components
-import { ShapeContainer } from '../Components/StyledComponents/StyledComponents'
+import { ShapeContainer } from '../Components/StyledComponents/StyledComponents';
+
+// Custom styled components.
+export const ProductCardsContainer = styled(ShapeContainer)`
+    /* products container gap */
+    gap: 1.5em;
+`
 
 function Products () {
+  /* const [data, setData] = useState([])
   
+  const fetchData = async () => {
+    let req = await fetch('https://fakestoreapi.com/products?limit=5')
+        .then(res=>res.json())
+        .then(json=>console.log(json))
+        setData({...req})
+    }
+
+  fetchData() */
+
   return (
-    <ShapeContainer>
-      <h1>Our Products</h1>
-      <ProductCards />
-    </ShapeContainer>
+    <>
+      <h1 style={{ paddingLeft: '50px'}}>Our Products</h1>
+      <ProductCardsContainer>
+          <ProductCards />
+          <ProductCards />
+          <ProductCards />
+          <ProductCards />
+          <ProductCards />
+          <ProductCards />
+          <ProductCards />
+          <ProductCards />
+          <ProductCards />
+          <ProductCards />
+          <ProductCards />
+          <ProductCards />
+      </ProductCardsContainer>
+    </>
   )
 }
 
