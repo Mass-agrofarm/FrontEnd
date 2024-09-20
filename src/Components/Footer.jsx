4c13e0d36dddd5ddcb9cb531e1f600aa54acad4a
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-09-20 23:06:40 +0300

    Feat: Added the footer component and some demo content
    ideal layout for the footer added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,12 +4,35 @@ import { FlexDiv } from './StyledComponents/StyledComponents'
 function Footer () {
   return (
     <>
-      <FlexDiv style={{ height: '60vh', backgroundColor: 'black' }}>
-        <FlexDiv style={{ flexDirection: 'row', alignItems: 'center'}}>
-            <div>Logo</div>
-            <div>Links</div>
+      <FlexDiv style={{ height: '60vh', backgroundColor: '#343434', justifyContent: 'center' }}>
+        <FlexDiv style={{ flexDirection: 'row',alignItems: 'center', padding: '96px', gap: '120px' }}>
+            
+            {/* logo */}
+            <div style={{alignItems: 'flex-start', width: '100%'}}>
+              <h4>Mass Agro Farm</h4>
+              <img style={{ width: '350px', height: '200px'}} src="../../public/kisspng-jcb-backhoe-loader-heavy-machinery-bulldozer-5b0849998b5248.6003143515272697855707-1024x595.png"/>
+            </div>
+
+            {/* links */}
+            <div>
+              <h4>Links</h4>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Products</li>
+                <li>Contact us</li>
+              </ul>
+            </div>
+
+            {/* social links */}
             <div>Socials</div>
-            <div>CTA</div>
+
+            {/* CTA, sub to newsletter */}
+            <FlexDiv style={{alignItems: 'flex-end', flexDirection: 'column', gap: '12px'}}>
+              <input type='email' style={{ padding: '8px 25px', borderRadius: '10px', border: 'none'}} placeholder='Subscribe to our newsletter'></input>
+              <button style={{ padding: '8px 25px', justifyContent: 'center' }}>Subscribe</button>
+            </FlexDiv>
+
         </FlexDiv>
       </FlexDiv>
     </>
