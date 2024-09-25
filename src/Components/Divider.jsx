e0c16f6dc5fd3718fe45449be7e6fc65f1cdf4f1
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-09-25 21:27:29 +0300

    Made the divider a component for ease and reuseability

---
+++ src/Components/Divider.jsx
@@ -0,0 +1,14 @@
+// Components
+import { FlexDiv } from './StyledComponents/StyledComponents'
+
+
+function Divider (){
+  return (
+
+    <FlexDiv style={{ justifyContent: 'center' }}>
+        <div style={{ width: '852px', border: '1px solid #ccc' }}></div>
+    </FlexDiv>
+  )
+}
+
+export default Divider
