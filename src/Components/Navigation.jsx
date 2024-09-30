ab57ebbaf574c540682dbc6c91b2c976605611f9
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-09-30 23:48:57 +0300

    Feat: Added the official use logo on the navigaion bar
    I'm left to figure out the styling for responsive design

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,20 +1,36 @@
 import { Link } from 'react-router-dom';
-import { NavBar, InnerContainer } from './StyledComponents/StyledContainers';
+import Logo from '../assets/logo.svg';
+import styled from 'styled-components';
 
+// components.
+import { NavBar, InnerContainer } from './StyledComponents/StyledContainers';
+import { LogoImage } from './StyledComponents/StyledElements';
+
+
+const LogoLink = styled(Link)`
+    padding: 0;
+    width: 40px;
+    height: 40px;
+    border: none;
+
+    &hover{
+      border-bottom: none;
+    }
+`
 
 
 
 const Navigation = () => {
   return (
     <NavBar>
-        <InnerContainer>
-            <Link to="/">
-              Logo
-            </Link>
-        </InnerContainer>
+        <>
+            <LogoLink to="/" >
+              <LogoImage src={Logo} alt="Mass AgroFarm logo" />
+            </LogoLink>
+        </>
         <InnerContainer>
             <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
+            <Link to="/about">About Us</Link>
             <Link to="/products">Products</Link>
             <Link to="/contacts">Contact us</Link>
         </InnerContainer>
--- src/Components/StyledComponents/StyledElements.jsx
+++ src/Components/StyledComponents/StyledElements.jsx
@@ -1,7 +1,17 @@
 /* eslint-disable react-refresh/only-export-components */
 import { styled } from 'styled-components';
 
-
+// paragraph styles
 export const P = styled.p`
-    
+    font-size: 18px;
+    font-weight: 400;
+`
+
+// image styles.
+
+export const LogoImage = styled.img`
+    height: 100px;
+    width: 100px;
+    position: absolute;
+    top: 0;
 `
