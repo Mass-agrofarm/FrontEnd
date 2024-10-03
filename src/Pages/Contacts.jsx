b94ad0816e15008d2f97f07dd8e6259319fa682c
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-10-03 18:07:50 +0300

    Feat: mods to the Contact page
    Modified the Contact page by including essential styled components

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -1,21 +1,43 @@
+// dependencies
+
+// components
 import { FlexDiv } from '../Components/StyledComponents/StyledContainers';
+import { ContactBanner, ContactSection } from '../Components/Contact/ContactStyledContainers';
+import Form from '../Components/Form';
 
 function Contacts (){
   return (
     <>
-      <section>
         <FlexDiv style={{ justifyContent: 'center' }}>
           <h1>Contact Us</h1>
         </FlexDiv>
-        <FlexDiv>
-          <p>
-            <a href="mailto:info@yourdomain.com">info@yourdomain.com</a>
-            <a href="http://">+3397449797</a>
-          </p>
-        </FlexDiv>
-        <FlexDiv>customer satisfaction and rating.</FlexDiv>
-        <footer>FAQ</footer>
-      </section>
+
+        {/* banner column */}
+        <ContactSection>
+          <ContactBanner>
+            <h3>Get in touch</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio ex non natus iusto dignissimos. Fugit nemo suscipit veniam? Amet iusto doloremque ab quia laudantium ipsam animi nemo quasi blanditiis esse?</p>
+            <span>
+              <h4>Emails</h4>
+              <a href="mailto:info@yourdomain.com">info@yourdomain.com</a>
+            </span>
+
+            <span>
+              <h4>Contact</h4>
+              <a href="http://">+3397449797</a>
+            </span>
+          </ContactBanner>
+
+          {/* contact form */}
+          <FlexDiv>
+            <Form />
+          </FlexDiv>
+          
+        </ContactSection>
+
+        {/* map location */}
+        <footer>map</footer>
+
     </>
   )
 }
