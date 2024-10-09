21f5283cd97e6bbc85027041306723480814ebfc
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-10-09 20:18:22 +0300

    Chore: Deletes to the app's entry point
    Deleted the import react declaration from the app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // Dependancies
 import './App.css';
-import { React } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import { OuterContainer } from './Components/StyledComponents/StyledComponents';
 
