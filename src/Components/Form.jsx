4a99f518218a187ab4880882cd25638b533ac421
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-10-11 00:41:56 +0300

    Chore: file path irresolved issue

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 
 // Components.
-import { NameFields, ContactFields, TextField } from './Contact/StyledContainers.jsx';
+import { NameFields, ContactFields, TextField } from '../Components/Contact/StyledContainers';
 import { FlexDiv } from './StyledComponents/StyledContainers';
 
 
