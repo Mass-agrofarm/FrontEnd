769a4ea49e82490977eaa96de1a501c32d2bc74a
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-10-11 15:32:03 +0300

    Chore: file path irresolved issue

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  //* absolute path configs.
+  resolve: {
+    alias: {
+      src: '/src',
+    }
+  },
+
+  //* Server configs.
   server: {
     port: 3000,
     strictPort: true,
