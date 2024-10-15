882e8dc32089340cee92750f07375b939eab6df4
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-10-15 22:40:14 +0300

    Styles: styles refactoring
    decoupling most of the style rules for components for ease in responsive design

---
+++ src/Components/Home/StyledContainers.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+
+export const FeaturedProductsSection = styled.section`
+    height: 105vh;
+    margin-bottom: 50px;
+    background-color: #38830c;
+`
---
+++ src/Components/ProductComponents/StyledContainers.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+import { ShapeContainer } from '@components/StyledComponents/StyledContainers';
+
+
+export const ProductCardsContainer = styled(ShapeContainer)`
+    /* products container gap */
+    gap: 1.5em;
+`
