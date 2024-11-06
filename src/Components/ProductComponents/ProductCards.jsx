efe889055252812750c3f1c3cd4443653da5f6f1
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-11-07 01:31:43 +0300

    Feat: added official whatsApp number
    Added the official use whatsApp contact to make product enquieries from product page through link to chat api by whatsApp

--- src/Components/ProductComponents/ProductCards.jsx
+++ src/Components/ProductComponents/ProductCards.jsx
@@ -29,7 +29,7 @@ const ProductCards = ({ data }) => {
             <div className="product-bottom">
               <P className="product-description">{product.description}</P>
               <div className="product-enquire-button">
-                <a href={`https://wa.me/254123456789?text=Hey%20there%2C%20%0AI'm%20texting%20to%20know%20more%20about%20the%20${product.name}%20posted%20on%20your%20site.%20feel%20free%20to%20contact%20me%20here%2C%20thank%20you%20for%20your%20time%20%F0%9F%98%81`} target='_blank' className="product-button"><FontAwesomeIcon icon={faWhatsapp} size='xl' style={{color: "#2ba64aff",}} /> Enquire</a>
+                <a href={`https://wa.me/254712519486?text=Hey%20there%2C%20%0AI'm%20texting%20to%20know%20more%20about%20the%20${product.name}%20posted%20on%20your%20site.%20feel%20free%20to%20contact%20me%20here%2C%20thank%20you%20for%20your%20time%20%F0%9F%98%81`} target='_blank' className="product-button"><FontAwesomeIcon icon={faWhatsapp} size='xl' style={{color: "#2ba64aff",}} /> Enquire</a>
               </div>
             </div>
           </div>
