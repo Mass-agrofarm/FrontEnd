e3316443774eb58bb6c5d9ec8541710d05e37ef2
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-11-15 18:21:14 +0300

    Feat: added the official social links to marsagrofarm socials

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -45,9 +45,9 @@ function Footer () {
               <div>
                 <h4 style={{ marginBottom: '0', marginTop: '0' }}>Socials</h4>
                 <FooterLinks>
-                  <a href="https://facebook.com" target='_blank'>facebook</a>
-                  <a href="https://x.com" target='_blank'>twitter</a>
-                  <a href="https://tiktok.com" target='_blank'>tiktok</a>
+                  <a href="https://www.facebook.com/stevenkingsenterprises" target='_blank'>facebook</a>
+                  <a href="https://x.com/MarsAgrofarm11?t=WsbEJsbG94pMEybLe_4afg&s=08" target='_blank'>twitter</a>
+                  <a href="https://www.tiktok.com/@marsagrofarm254?_t=8rEKbOI0AZs&_r=1" target='_blank'>tiktok</a>
                   <a href="https://insagram.com" target='_blank'>instagram</a>
                 </FooterLinks>
               </div>
