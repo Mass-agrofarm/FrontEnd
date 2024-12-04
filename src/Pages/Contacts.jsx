5ed74e9cdab8a906da448b6c6a423e7a82cec522
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-12-04 21:50:31 +0300

    Chore: replaced the map detail
    Added the ideal location on the contacts page map preview (Tala)

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -57,7 +57,7 @@ function Contacts (){
 
         {/* map location */}
         <MapFooter>
-          <iframe style={{ width: "100%" ,height: "300px" ,border: "0"}} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d510564.65107988653!2d36.5177334104462!3d-1.3031873859975642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1172d84d49a7%3A0xf7cf0254b297924c!2sNairobi!5e0!3m2!1sen!2ske!4v1728150613425!5m2!1sen!2ske"  allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.842068817304!2d37.311484670191504!3d-1.2675082327517866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x18258a1f2b26e003%3A0xb6a5966328a095f6!2sSTEVENKINGS%20ENTERPRISES!5e0!3m2!1sen!2ske!4v1733333870295!5m2!1sen!2ske" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </MapFooter>
 
     </>
